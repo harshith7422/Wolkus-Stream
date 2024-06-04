2515963971afc80ce1a60234ed7bfed09cadde8b
Harshith YVS <89782073+harshith7422@users.noreply.github.com>
2024-06-04 15:45:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
       <div>
         <nav className="navbar navbar-expand-lg bg-body-tertiary">
           <div className="container-fluid">
-            <Link className="navbar-brand" to="/">Movie Library</Link>
+            <Link className="navbar-brand" to="/">Wolkus Stream</Link>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
